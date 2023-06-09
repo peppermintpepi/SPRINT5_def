07b467124de5b05528bc1e118c56dc3981b859fa
peppermintpepi <pbalmana@icloud.com>
2023-06-09 05:36:46 +0200

    first commit

---
+++ jokes.js
@@ -0,0 +1,75 @@
+var reportAcudits = [];
+var actualJoke;
+
+function JokeExist() {
+    var jokeExist = reportAcudits.filter(function (item) {
+        return item.joke == actualJoke;
+    });
+    if (jokeExist.length === 0)
+        return false;
+    else
+        return true;
+}
+
+function JokeRated() {
+    var jokeRated = reportAcudits.filter(function (item) {
+        return item.joke == actualJoke && item.score > 0;
+    });
+    if (jokeRated.length === 0)
+        return false;
+    else
+        return true;
+}
+
+function buttonsDisplay() {
+    var jokesText = document.getElementById('actualJoke');
+    if (jokesText.innerText != " ") {
+        document.getElementById('score-dad-jokes').style.display = 'block';
+    }
+}
+
+
+function getJoke() {
+    console.log("dad_joke");
+    fetch('https://icanhazdadjoke.com/', {
+            method: "GET",
+            headers: {
+                "Accept": 'application/json'
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            document.getElementById("actualJoke").innerText = json.joke;
+            actualJoke = json.joke;
+            // console.log(json);
+        })
+        .then(function () {
+            return buttonsDisplay();
+        })["catch"](function () {
+            console.log("Error en la llamada a la API");
+        });
+}
+
+function NextImage(index) {
+    var div_main = document.getElementById("main");
+    var url = "./images/blob-" + index + ".svg";
+    div_main.style.backgroundImage = 'url(' + url + ')';
+    console.log(url);
+}
+
+function Score(userScore) {
+    actualJoke = document.getElementById("actualJoke").innerText;
+    var d = new Date();
+    var sDate = d.toISOString();
+    if (actualJoke != "") {
+        reportAcudits.push({
+            joke: actualJoke,
+            score: userScore,
+            date: sDate
+        });
+        buttonsDisplay();
+        console.log(reportAcudits);
+    }
+}
